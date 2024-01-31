c57375421d9fb631310037e583d4fa9b2d086683
aaaa1597 <tokushi2011@gmail.com>
2024-01-31 21:42:01 +0900

    出来た。完成

--- reactts/src/App.tsx
+++ reactts/src/App.tsx
@@ -1,84 +1,75 @@
 import React, { useEffect, useRef } from 'react';
 import './App.css';
 
-const constraints: MediaStreamConstraints = {
-  audio: false,
-  video: {
-    // width:  { min: 1280, ideal: 1920, max: 2560 },
-    // height: { min: 720 , ideal: 1080, max: 1440 } ,
-    width:  640,
-    height: 480 ,
-    // frameRate: {
-    //   max: 30,
-    // }//,
-    // facingMode: {
-    //   exact: 'environment',
-    // },
-  },
- };
+class point {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  toString() {
+    return '('+this.x+','+this.y+')';
+  }
+}
+
+class rect {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+  constructor(x: number, y: number, w: number, h: number) {
+    this.x=x;
+    this.y=y;
+    this.w=w;
+    this.h=h;
+  }
+  toString() {
+    return '('+this.x+','+this.y+','+this.w+','+this.h+')';
+  }
+}
 
 function App() {
   const refWasm = useRef<any>(null)
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const ctxRef = useRef<CanvasRenderingContext2D|null>();
-  const loopRef = useRef(0);
-  let cnt = 0;
+  const refTextin = useRef<HTMLTextAreaElement>(null!)
+  const refTextout = useRef<HTMLTextAreaElement>(null!)
 
   /* wasm読込み */
   const h = require('./cppmain.js');
   h.Module.onRuntimeInitialized = () => {
     console.log("Wasm loaded.");
-    h.Module["canvas"] = canvasRef.current
     refWasm.current = h.Module
   }
- 
-  /* 初期化 */
+
   useEffect(() => {
-    const openCamera = async () => {
-      const video = videoRef.current;
-      const canvas = canvasRef.current;
-      if (video && canvas) {
-        canvas.width = 640;
-        canvas.height = 480;
-        video.width = 640;
-        video.height = 480;
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        video.srcObject = stream;
-        video.play();
-        ctxRef.current = canvas.getContext('2d');
-        loop();
-      }
-    };
-    openCamera();
+    refTextin.current.rows = 5
+    refTextout.current.rows = 5
+  });
 
-    return () => cancelAnimationFrame(loopRef.current);
-  }, [])
-
-  /* ループ */
-  const loop = () => {
-    const ctx = ctxRef.current;
-    const video = videoRef.current;
-    const canvas = canvasRef.current;
+  const bclick = () => {
+    const textout = refTextout.current;
     const wasm = refWasm.current;
-    if(ctx && video && canvas && wasm) {
-      ctx.drawImage(video, 0, 0);
-      const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      const buffer = wasm._creata_buffer(data.data.length);
-      wasm.HEAPU8.set(data.data, buffer);
-      wasm._Convert(buffer, data.width, data.height, cnt);
-      wasm._destroy_buffer(buffer);
-      cnt++;
-      if(cnt>=200) cnt = 0;
-    }
-    loopRef.current = requestAnimationFrame(loop);
+    if(!textout && !wasm) return;
+		textout.value ="f1(1,2)=" + wasm.f1(1,2) + "\n";
+		textout.value+="f2(2.5,3.5)=" + wasm.f2(2.5, 3.5) + "\n";
+		textout.value+="f3('aaa', 'bbbb')=" + wasm.f3('aaa', 'bbbb') + "\n";
+		const arg41=[1,2,3,4], arg42=[5,6,7,8];
+		textout.value+="f4([1,2,3,4], [5,6,7,8])=" + wasm.f4(arg41, arg42) + "\n";
+		const arg51=new Uint8Array([1,2,3,4]), arg52=new Uint8Array([5,6,7,8]);
+		textout.value+="f5(new Uint8Array([1,2,3,4]), new Uint8Array([5,6,7,8]))=" + wasm.f5(arg51, arg52) + "\n";
+		const arg61=['aa','bb','cc','dd'], arg62=['ee','ff','gg','hh'];
+		textout.value+="f6(['aa','bb','cc','dd'], ['ee','ff','gg','hh'])=" + wasm.f6(arg61, arg62) + "\n";
+		const arg71=new point(1,2), arg72=new point(3,4);
+//		textout.value+="f7(new point(1,2), new point(3,4))=" + wasm.f7(arg71, arg72) + "\n";
+		textout.value+="f8({x2:1, y2:2},{w2:3, h2:4})=" + JSON.stringify(wasm.f8({x2:1, y2:2},{w2:3, h2:4}));
   }
 
   return (
     <div className="App">
       hello world!!
-      <video autoPlay playsInline={true} ref={videoRef} />
-      <canvas width="640" height="480" ref={canvasRef} />
+      <button onClick={bclick}>Click</button>
+      <textarea id="textin" ref={refTextin}></textarea>
+      <textarea id="textout" ref={refTextout}></textarea>
     </div>
   );
 }
